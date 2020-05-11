c0564ebf0505ba162a72d65471f8854062bba324
MartyMark <burghardt.marty@web.de>
2020-05-12 00:01:32 +0200

    Refactor

--- src/main/java/eureka/Application.java
+++ src/main/java/eureka/Application.java
@@ -1,4 +1,4 @@
-package eurika;
+package eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
